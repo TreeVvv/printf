0b77d38d0ad89ec9459e72fbbcdba6ef8b4a1311
Raymond Enokela <raydiamond316@gmail.com>
2023-10-17 13:09:18 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,59 +1,45 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int count = 0;  // Initialize character count to zero
+	va_list args;
+	int i = 0, j, len = 0;
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;  // Move past '%'
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-            if (*format == '\0') {
-                break;  // Reached the end of the format string
-            }
-
-            if (*format == 'c') {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            } else if (*format == 's') {
-                const char *s = va_arg(args, const char *);
-                if (s == NULL) {
-                    s = "(null)";
-                }
-                while (*s != '\0') {
-                    putchar(*s);
-                    s++;
-                    count++;
-                }
-            } else if (*format == 'd' || *format == 'i') {
-                int num = va_arg(args, int);
-                printf("%d", num);
-                count++;
-            } else if (*format == '%') {
-                putchar('%');
-                count++;
-            } else {
-                // Unsupported format specifier, just print it as is
-                putchar('%');
-                putchar(*format);
-                count += 2;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
-}
-
-int main() {
-    int n = _printf("Hello, %s! The answer is %d.\n", "world", 42);
-    printf("Character count: %d\n", n);
-    return 0;
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
 }
